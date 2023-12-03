440b0d3e1b55b5040dceca52e14fee9a64627323
SandhraShaji <sandhrashajik@gmail.com>
2023-12-03 11:37:27 +0530

    update

--- script.js
+++ script.js
@@ -20,28 +20,33 @@ function regis(){
             alert("Details added succesfully.")
             window.location="./index.html"
         }
+        uname.value=""
+        em.value=""
+        psw.value=""
     }
-    uname.value=""
-    em.value=""
-    psw.value=""
 }
 function login(){
-    if(em1.value in localStorage){
-        let user1 = JSON.parse(localStorage.getItem(em1.value))
-        if(user1.psw == psw1.value){
-            sessionStorage.setItem("users",JSON.stringify(user1))
-            alert('Login successfull.')
-            window.location="./home.html"
-        }
-        else{
-            alert('Login failed! Wrong password.')
-        }
+    if(em1.value==""||psw1.value==""){
+        alert("Please enter all details!")
     }
     else{
-        alert('Login failed! Wrong email.')
-        em1.value=""
+        if(em1.value in localStorage){
+            let user1 = JSON.parse(localStorage.getItem(em1.value))
+            if(user1.psw == psw1.value){
+                sessionStorage.setItem("users",JSON.stringify(user1))
+                alert('Login successfull.')
+                window.location="./home.html"
+            }
+            else{
+                alert('Login failed! Wrong password.')
+            }
+        }
+        else{
+            alert('Login failed! Wrong email.')
+            em1.value=""
+        }
+        psw1.value=""
     }
-    psw1.value=""
 }
 var usern = JSON.parse(sessionStorage.getItem("users"))
 headi.innerHTML = `Welcome ${usern.uname}`
@@ -66,59 +71,67 @@ for(let i of usern.expense){
     exdet.innerHTML+=htdata
 }
 function income(){
-    var useri = JSON.parse(sessionStorage.getItem("users"))
-    let user2 = JSON.parse(localStorage.getItem(useri.em))
-    const date = new Date().toString().slice(4,15)
-    inc = {
-        type:desi.value,
-        amtt:amti.value,
-        date:date
+    if(desi.value==""||amti.value==""){
+        alert("Please fill both feilds.")
     }
-    user2.income.push(inc)
-    user2.saving+=Number(amti.value)
-    alert("Income added successfully.")
-    dispbal.innerHTML=user2.saving
-    dispsp.innerHTML=user2.spent  
-    localStorage.setItem(user2.em,JSON.stringify(user2))
-    sessionStorage.setItem("users",JSON.stringify(user2))
-    desi.value=""
-    amti.value=""
-    indet.innerHTML+=`
-        <tr>
-            <td>${inc.type}</td>
-            <td>${inc.amtt}</td>
-            <td>${inc.date}</td>
-            <td>${user2.saving}</td>
-        </tr>
-    `
+    else{
+        var useri = JSON.parse(sessionStorage.getItem("users"))
+        let user2 = JSON.parse(localStorage.getItem(useri.em))
+        const date = new Date().toString().slice(4,15)
+        inc = {
+            type:desi.value,
+            amtt:amti.value,
+            date:date
+        }
+        user2.income.push(inc)
+        user2.saving+=Number(amti.value)
+        alert("Income added successfully.")
+        dispbal.innerHTML=user2.saving
+        dispsp.innerHTML=user2.spent  
+        localStorage.setItem(user2.em,JSON.stringify(user2))
+        sessionStorage.setItem("users",JSON.stringify(user2))
+        desi.value=""
+        amti.value=""
+        indet.innerHTML+=`
+            <tr>
+                <td>${inc.type}</td>
+                <td>${inc.amtt}</td>
+                <td>${inc.date}</td>
+                <td>${user2.saving}</td>
+            </tr>`
+    }
 }
 function expense(){
-    var usere = JSON.parse(sessionStorage.getItem("users"))
-    let user3 = JSON.parse(localStorage.getItem(usere.em))
-    const date = new Date().toString().slice(4,15)
-    exp = {
-        type:dese.value,
-        amtt:amte.value,
-        date:date
+    if(dese.value==""||amte.value==""){
+        alert("Please fill both feilds.")
     }
-    user3.expense.push(exp)
-    user3.saving-=Number(amte.value)
-    user3.spent+=Number(amte.value)
-    alert("Expense added successfully.")
-    dispbal.innerHTML=user3.saving
-    dispsp.innerHTML=user3.spent  
-    localStorage.setItem(user3.em,JSON.stringify(user3))
-    sessionStorage.setItem("users",JSON.stringify(user3))
-    dese.value=""
-    amte.value=""
-    exdet.innerHTML+=`
-        <tr>
-            <td>${exp.type}</td>
-            <td>${exp.amtt}</td>
-            <td>${exp.date}</td>
-            <td>${user3.saving}</td>
-        </tr>
-    `
+    else{
+        var usere = JSON.parse(sessionStorage.getItem("users"))
+        let user3 = JSON.parse(localStorage.getItem(usere.em))
+        const date = new Date().toString().slice(4,15)
+        exp = {
+            type:dese.value,
+            amtt:amte.value,
+            date:date
+        }
+        user3.expense.push(exp)
+        user3.saving-=Number(amte.value)
+        user3.spent+=Number(amte.value)
+        alert("Expense added successfully.")
+        dispbal.innerHTML=user3.saving
+        dispsp.innerHTML=user3.spent  
+        localStorage.setItem(user3.em,JSON.stringify(user3))
+        sessionStorage.setItem("users",JSON.stringify(user3))
+        dese.value=""
+        amte.value=""
+        exdet.innerHTML+=`
+            <tr>
+                <td>${exp.type}</td>
+                <td>${exp.amtt}</td>
+                <td>${exp.date}</td>
+                <td>${user3.saving}</td>
+            </tr>`
+    }
 }
 function logout(){
     sessionStorage.clear()
